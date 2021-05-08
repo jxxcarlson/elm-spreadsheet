945b9119d6f172d8e61bc4e2f5d6dc2c2495cc1b
James Carlson <jxxcarlson@gmail.com>
2021-05-08 07:03:53 -0400

    Add module Solve with function treesFromSpreadsheet : List (List (Cell)) -> List (Tree (Int, Int))

---
+++ src/Solve.elm
@@ -0,0 +1,111 @@
+module Solve exposing (..)
+
+import Cell exposing (Cell, Col, Formula(..), Row, Value(..))
+import Either exposing (Either(..))
+import List.Extra
+import Maybe.Extra
+import Spreadsheet exposing (Spreadsheet)
+import TestData
+import Tree exposing (Tree(..), singleton, tree)
+
+
+
+-- TREES FROM SPREADSHEET
+
+
+treesFromSpreadsheet sheet =
+    sheet
+        |> augmentSpreadsheet
+        |> List.map (List.map treeFromCAugmentedCell)
+        |> List.map Maybe.Extra.values
+        |> List.concat
+
+
+cellAt : Row -> Col -> List (List a) -> Maybe a
+cellAt row col list =
+    List.Extra.getAt col list |> Maybe.andThen (List.Extra.getAt row)
+
+
+augmentCells : List Cell -> List ( Int, Cell )
+augmentCells cells =
+    List.indexedMap (\k cell -> ( k, cell )) cells
+
+
+augmentColumnsOfSpreadsheet : List (List Cell) -> List (List ( Int, Cell ))
+augmentColumnsOfSpreadsheet sheet =
+    List.map augmentCells sheet
+
+
+augmentRowOfSpreadsheet : Int -> List ( Int, Cell ) -> List ( ( Int, Int ), Cell )
+augmentRowOfSpreadsheet i list =
+    List.map (\( j, cell ) -> ( ( j, i ), cell )) list
+
+
+augmentRowsOfSpreadsheet : List (List ( Int, Cell )) -> List (List ( ( Int, Int ), Cell ))
+augmentRowsOfSpreadsheet list =
+    List.indexedMap (\row cells -> augmentRowOfSpreadsheet row cells) list
+
+
+augmentSpreadsheet : List (List Cell) -> List (List ( ( Int, Int ), Cell ))
+augmentSpreadsheet list =
+    list |> augmentColumnsOfSpreadsheet |> augmentRowsOfSpreadsheet
+
+
+treeFromCAugmentedCell : ( ( Row, Col ), Cell ) -> Maybe (Tree ( Int, Int ))
+treeFromCAugmentedCell ( ( row, col ), cell ) =
+    case cell of
+        Right _ ->
+            Nothing
+
+        Left formula ->
+            treeFromFormula row col formula
+
+
+treeFromFormula : Row -> Col -> Formula -> Maybe (Tree ( Int, Int ))
+treeFromFormula row col formula =
+    case formula of
+        RowOp op i j ->
+            if List.member op [ "+", "-", "*", "/" ] then
+                Just (tree ( row, col ) [ singleton ( row, i ), singleton ( row, j ) ])
+
+            else
+                Nothing
+
+        ColOp op i j ->
+            if List.member op [ "sum" ] then
+                Just (tree ( row, col ) (singletonsWithRowRange col i j))
+
+            else
+                Nothing
+
+
+singletonsWithRowRange : Col -> Row -> Row -> List (Tree ( Int, Int ))
+singletonsWithRowRange col row1 row2 =
+    List.map (\row -> singleton ( row, col )) (List.range row1 row2)
+
+
+{-| -}
+
+
+
+-- TEST
+
+
+t1 =
+    tree ( 0, 2 ) [ singleton ( 0, 0 ), singleton ( 0, 1 ) ]
+
+
+t2 =
+    tree ( 1, 2 ) [ singleton ( 0, 0 ), singleton ( 0, 1 ) ]
+
+
+t3 =
+    tree ( 2, 2 ) [ singleton ( 0, 0 ), singleton ( 0, 1 ) ]
+
+
+t4 =
+    tree ( 3, 2 ) [ singleton ( 0, 2 ), singleton ( 1, 2 ), singleton ( 2, 2 ) ]
+
+
+t5 =
+    tree ( 3, 1 ) [ singleton ( 0, 1 ), singleton ( 1, 1 ), singleton ( 1, 2 ) ]
--- src/TestData.elm
+++ src/TestData.elm
@@ -8,24 +8,12 @@ import Either exposing (Either(..))
 -- TEST DATA
 
 
-testSheetCol1 =
-    [ Right (Real 100), Right (Real 120), Right (Real 140), Right Undefined ]
-
-
 textCol1 =
     [ "100.0", "120.0", "140.0", "-" ]
 
 
-testSheetCol2 =
-    [ Right (Real 1.1), Right (Real 1.4), Right (Real 0.9), Left (ColOp "col sum" 1 3) ]
-
-
 textCol2 =
     [ "1.1", "1.4", "0.9", "col sum 1 3" ]
-
-
-testSheetCol3 =
-    [ Left (RowOp "*" 1 2), Left (RowOp "*" 1 2), Left (RowOp "*" 1 2), Left (ColOp "col sum" 1 3) ]
 
 
 textCol3 =
@@ -33,7 +21,7 @@ textCol3 =
 
 
 ast =
-    [ testSheetCol1, testSheetCol2, testSheetCol3 ]
+    [ [ Right (Real 100), Right (Real 120), Right (Real 140), Right Undefined ], [ Right (Real 1.1), Right (Real 1.4), Right (Real 0.9), Left (ColOp "sum" 0 2) ], [ Left (RowOp "*" 0 1), Left (RowOp "*" 0 1), Left (RowOp "*" 0 1), Left (ColOp "sum" 0 2) ] ]
 
 
 text =
